18034bd5d8eefedd04e82465f1979ccaa491dd3e
Tom Price <t0mpr1c3@gmail.com>
2025-06-24 17:45:33 -0400

    use socket option SO_REUSEPORT

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -15,13 +15,6 @@
 #include <errno.h>
 
 
-// shared resource object
-// see https://stackoverflow.com/questions/14888027/mutex-lock-threads
-struct bound_socket {
-	pthread_mutex_t mutex;
-	int sockfd;
-};
-
 #define false 0
 #define true 1
 #define PORT "9000" 
@@ -29,20 +22,9 @@ struct bound_socket {
 #define BUFFER_SIZE 1000
 #define DIRNAME "/var/tmp/"
 #define FILENAME "/var/tmp/aesdsocketdata"
-#define SHM_HANDLE "/BOUND_SOCKET"
+#define IPC_HANDLE "/AF_UNIX_SOCKETPAIR"
 
 static int sig = 0;
-
-const static struct linger linger00 = { 0, 0 };
-
-/*
-// path names
-const char *dirname = "/var/tmp/";
-const char *filename = "/var/tmp/aesdsocketdata";
-
-// domain name
-const char *localhost = "localhost";
-*/
 
 // get IP address from addrinfo and convert to a string
 void ip2str (struct addrinfo *ai, char *buf) {
@@ -71,7 +53,7 @@ void signal_handler (int s) {
 int main (int argc, char *argv[]) {
 	syslog(LOG_DEBUG, "Starting aesdsocket\n");
 
-	int status, shm, sd2, fd, mask, daemon;
+	int status, sd, sd2, fd, mask, daemon;
 	struct sigaction sa;
 	struct addrinfo hints, *servinfo;
 	struct sockaddr_storage saddr;
@@ -83,7 +65,6 @@ int main (int argc, char *argv[]) {
 	size_t len;
 	off_t file_offset;
 	pid_t process;
-	struct bound_socket *bs;
 
 	if (argc == 1) {
 		daemon = false;
@@ -128,26 +109,6 @@ int main (int argc, char *argv[]) {
 	}
 	syslog(LOG_DEBUG, "%s is absent", FILENAME);
 
-	// map shared memory
-	shm = shm_open(SHM_HANDLE, O_CREAT|O_TRUNC|O_RDWR, 0600);
-	if (shm < 0) {
-		perror("shm_open");
-		syslog(LOG_ERR, "shm_open: %s", strerror(errno));
-		exit(-1);
-	}
-	syslog(LOG_DEBUG, "Created shared memory handle %s", SHM_HANDLE);
-	syslog(LOG_DEBUG, "Opened shared memory descriptor shm=%d", shm);
-	ftruncate(shm, sizeof(struct bound_socket));
-	bs = (struct bound_socket*) mmap(0, sizeof(struct bound_socket), PROT_READ|PROT_WRITE, MAP_SHARED, shm, 0);
-
-	// make sure mutex can be shared across processes
-	pthread_mutexattr_t shared;
-	pthread_mutexattr_init(&shared);
-	pthread_mutexattr_setpshared(&shared, PTHREAD_PROCESS_SHARED);
-
-	// initialize mutex
-	pthread_mutex_init(&(bs->mutex), &shared);
-
 	// initialize hints
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
@@ -170,38 +131,31 @@ int main (int argc, char *argv[]) {
 	ip2str(servinfo, ipstr);
 
 	// create socket
-	bs->sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-	if (bs->sockfd < 0) {
+	sd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+	if (sd < 0) {
 		perror("socket");
 		syslog(LOG_ERR, "socket: %s", strerror(errno));
 		exit(-1);
 	}
-	status = setsockopt(bs->sockfd, SOL_SOCKET, SO_LINGER, (void*) &linger00, sizeof(struct linger));
+	syslog(LOG_DEBUG, "Opened shared socket file descriptor sd=%d", sd);
+
+	// set socket option SO_REUSEPORT
+	int optval = 1;
+	status = setsockopt(sd, SOL_SOCKET, SO_REUSEPORT, (void*) &optval, sizeof(optval));
 	if (status < 0) {
 		perror("setsockopt");
 		syslog(LOG_ERR, "setsockopt: %s", strerror(errno));
 		exit(-1);
 	}
-	syslog(LOG_DEBUG, "Opened shared socket file descriptor bs->sockfd=%d", bs->sockfd);
-
-	// obtain mutex
-	// prevents multiple threads from attempting to connect to the same port
-	status = pthread_mutex_lock(&(bs->mutex));
-	if (status < 0) {
-		perror("pthread_mutex_lock");
-		syslog(LOG_ERR, "pthread_mutex_lock: %s", strerror(errno));
-		close(bs->sockfd);
-		exit(-1);
-	}
-	syslog(LOG_DEBUG, "Obtained mutex=%p\n", &(bs->mutex));
+	syslog(LOG_DEBUG, "Set option SO_REUSEPORT for socket file descriptor sd=%d", sd);
 
 	// bind socket to port
-	status = bind(bs->sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
+	status = bind(sd, servinfo->ai_addr, servinfo->ai_addrlen);
 	if (status < 0) {
 		perror("bind");
 		syslog(LOG_ERR, "bind: %s", strerror(errno));
-		syslog(LOG_ERR, "bind arguments: %d %p %u\n", bs->sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
-		close(bs->sockfd);
+		syslog(LOG_ERR, "bind arguments: %d %p %u\n", sd, servinfo->ai_addr, servinfo->ai_addrlen);
+		close(sd);
 		exit(-1);
 	}
 
@@ -211,21 +165,21 @@ int main (int argc, char *argv[]) {
 		if (process < 0) {
 			perror("fork");
 			syslog(LOG_ERR, "fork: %s", strerror(errno));
-			close(bs->sockfd);
+			close(sd);
 			exit(-1);
 		}
 		if (process != 0) { // parent
-			close(bs->sockfd);
+			close(sd);
 			exit(0);
 		}
 	}
 
 	// listen on port
-	status = listen(bs->sockfd, BACKLOG);
+	status = listen(sd, BACKLOG);
 	if (status < 0) {
 		perror("listen");
 		syslog(LOG_ERR, "listen: %s", strerror(errno));
-		close(bs->sockfd);
+		close(sd);
 		exit(-1);
 	}
 
@@ -236,14 +190,14 @@ int main (int argc, char *argv[]) {
         if (status != 0) {
 		perror("sigaction");
 		syslog(LOG_ERR, "sigaction: %s", strerror(errno));
-		close(bs->sockfd);
+		close(sd);
 		exit(-1);
 	}	
 	status = sigaction(SIGTERM, &sa, NULL);
         if (status != 0) {
 		perror("sigaction");
 		syslog(LOG_ERR, "sigaction: %s", strerror(errno));
-		close(bs->sockfd);
+		close(sd);
 		exit(-1);
 	}	
 	sig = 0;
@@ -253,11 +207,11 @@ int main (int argc, char *argv[]) {
 
 		// accept connection
 		saddrlen = sizeof saddr;
-		sd2 = accept(bs->sockfd, (struct sockaddr*) &saddr, &saddrlen);
+		sd2 = accept(sd, (struct sockaddr*) &saddr, &saddrlen);
 		if (sd2 < 0) {
-			perror("socket");
-			syslog(LOG_ERR, "socket: %s", strerror(errno));
-			close(bs->sockfd);
+			perror("accept");
+			syslog(LOG_ERR, "accept: %s", strerror(errno));
+			close(sd);
 			exit(-1);
 		}
 		printf("sd2=%d\n",sd2);
@@ -271,7 +225,7 @@ int main (int argc, char *argv[]) {
 		if (fd < 0) {
 			perror("open");
 			syslog(LOG_ERR, "open: %s", strerror(errno));
-			close(bs->sockfd);
+			close(sd);
 			exit(-1);
 		}
 		printf("fd=%d\n",fd);
@@ -286,7 +240,7 @@ int main (int argc, char *argv[]) {
 			if (bytes < 0) {
 				perror("recv");
 				syslog(LOG_ERR, "recv: %s", strerror(errno));
-				close(bs->sockfd);
+				close(sd);
 				exit(-1);
 			}
 			buf[bytes] = 0;
@@ -301,7 +255,7 @@ int main (int argc, char *argv[]) {
 				len = p - buf + 1;
 				if (len < bytes) {
 					fprintf(stderr, "packet format error");
-					close(bs->sockfd);
+					close(sd);
 					exit(-1);
 				}
 			}
@@ -313,19 +267,19 @@ int main (int argc, char *argv[]) {
 			if (bytes < 0) {
 				perror("write");
 				syslog(LOG_ERR, "write: %s", strerror(errno));
-				close(bs->sockfd);
+				close(sd);
 				exit(-1);
 			}
 			if (bytes < len) {
 				fprintf(stderr, "write error");
 				syslog(LOG_ERR, "write error");
-				close(bs->sockfd);
+				close(sd);
 				exit(-1);
 			}
 			if (status < 0) {
 				perror("close");
 				syslog(LOG_ERR, "close: %s", strerror(errno));
-				close(bs->sockfd);
+				close(sd);
 				exit(-1);
 			}
 			syslog(LOG_DEBUG, "Wrote %ld bytes to file", bytes);
@@ -347,7 +301,7 @@ int main (int argc, char *argv[]) {
 			if (file_offset < 0) {
 				perror("lseek");
 				syslog(LOG_ERR, "lseek: %s", strerror(errno));
-				close(bs->sockfd);
+				close(sd);
 				exit(-1);
 			}
 			printf("file offset = %ld\n", file_offset);
@@ -360,7 +314,7 @@ int main (int argc, char *argv[]) {
 			if (bytes < 0) {
 				perror("read");
 				syslog(LOG_ERR, "read: %s", strerror(errno));
-				close(bs->sockfd);
+				close(sd);
 				exit(-1);
 			} else if (bytes == 0) {
 				printf("EOF\n");
@@ -373,7 +327,7 @@ int main (int argc, char *argv[]) {
 			if (bytes < 0) {
 				perror("send");
 				syslog(LOG_ERR, "send: %s", strerror(errno));
-				close(bs->sockfd);
+				close(sd);
 				exit(-1);
 			}
 			printf("sent %ld bytes\n", bytes);
@@ -385,7 +339,7 @@ int main (int argc, char *argv[]) {
 		if (status < 0) {
 			perror("close");
 			syslog(LOG_ERR, "close: %s", strerror(errno));
-			close(bs->sockfd);
+			close(sd);
 			exit(-1);
 		}
 		printf("closed fd\n");
@@ -394,7 +348,7 @@ int main (int argc, char *argv[]) {
 		if (status < 0) {
 			perror("close");
 			syslog(LOG_ERR, "close: %s", strerror(errno));
-			close(bs->sockfd);
+			close(sd);
 			exit(-1);
 		}
 		printf("closed sd2\n");
@@ -415,34 +369,21 @@ int main (int argc, char *argv[]) {
 	syslog(LOG_DEBUG, "Caught signal, exiting");
 	printf("Caught signal, exiting");
 
-	status = close(bs->sockfd);
+	status = close(sd);
 	if (status < 0) {
 		perror("close");
 		syslog(LOG_ERR, "close: %s", strerror(errno));
 		exit(-1);
 	}
 	printf("closed sd\n");
-	syslog(LOG_DEBUG, "Closed socket file descriptor bs->sockfd=%d\n", bs->sockfd);
+	syslog(LOG_DEBUG, "Closed socket file descriptor sd=%d\n", sd);
 
+	/*
 	// sleep to allow TCP connection to close
 	syslog(LOG_DEBUG, "Start long sleep");
 	usleep(3000000);
 	syslog(LOG_DEBUG, "End long sleep");
-
-	// release mutex
-	status = pthread_mutex_unlock(&(bs->mutex));
-	if (status < 0) {
-		perror("pthread_mutex_unlock");
-		syslog(LOG_ERR, "pthread_mutex_unlock: %s", strerror(errno));
-		exit(-1);
-	}
-	syslog(LOG_DEBUG, "Released mutex=%p\n", &(bs->mutex));
-
-	// unmap shared memory
-	munmap(bs, sizeof(struct bound_socket));
-	syslog(LOG_DEBUG, "Close shared memory descriptor shm=%d\n", shm);
-	shm_unlink(SHM_HANDLE);
-	syslog(LOG_DEBUG, "Unlinked shared memory with handle %s\n", SHM_HANDLE);
+	*/
 
 	// free addrinfo
 	freeaddrinfo(servinfo);
